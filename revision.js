291371534a004fd51de598527994c626baa66beb
Ishtiaque1997 <ishtiaque07031997@gmail.com>
2023-06-07 22:48:44 +0600

    first commit

---
+++ revision.js
@@ -0,0 +1,34 @@
+//string type variable
+
+var bottleContent='water';
+var bottleColor='yellow';
+//number type variable
+var sunglassPrice=120;
+var penColor='blue';
+
+//boolean type variable
+var isExpensive=true;
+
+//array index starts with 0
+//if an element does not exist then index is -1.
+var tableItems= ['books','copies','pens','pencils'];
+
+var item3=tableItems[3];
+tableItems[3]='pencils';
+var position=tableItems.indexOf('pens');
+
+//push-pop operations. 
+
+//conditionals
+if(tableItems.length>4){
+
+}
+else if(tableItems>5){
+
+}
+else if(tableItems>9){
+
+}
+else{
+
+}
