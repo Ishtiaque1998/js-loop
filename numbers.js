311de2f84df74f7c4a397b45152a20ff4f419e40
Ishtiaque1997 <ishtiaque07031997@gmail.com>
2023-06-08 11:14:35 +0600

    more done

---
+++ numbers.js
@@ -0,0 +1,8 @@
+
+//Display 1 to 10
+var number=1;
+
+while(number<=10){
+ console.log(number);
+ number=number+1;
+}
---
+++ while-loop.js
@@ -0,0 +1,14 @@
+// var num1=21;
+// var num2=22;
+
+// while (num1<num2) {
+//  console.log('kisu hoilona');
+//  break;
+// }
+
+var roastGiven=0;
+while(roastGiven<7){
+ console.log('nice');
+ console.log(roastGiven)
+ roastGiven++;
+}
