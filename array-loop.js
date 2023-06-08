eaa2ddb6f45616fdb7ddb3b45e9a6435d6cae6bf
Ishtiaque1997 <ishtiaque07031997@gmail.com>
2023-06-08 11:48:26 +0600

    more done

---
+++ array-loop.js
@@ -0,0 +1,13 @@
+var numbers=[11,22,33,44,55,98,780];
+
+for (var i=0;i<numbers.length;i++){
+ var number= numbers[i];
+ console.log(number)
+}
+
+
+var items=['pen','pencil','rubber'];
+for(var i=0;i<=items.length;i++){
+ var item= items[i];
+ console.log(item)
+}
---
+++ for.js
@@ -0,0 +1,19 @@
+var roastGiven=0;
+
+for(roastGiven=0;roastGiven<10;roastGiven++){
+ // console.log(roastGiven)
+}
+
+//simple version of for loop
+
+// for(var i=0;i<=20;i=i+2){
+//  console.log(i);
+// }
+
+for (var i=0; i<=10;i=i+2){
+ console.log(i);
+}
+
+for (var i=1; i<=10;i=i+2){
+ console.log(i);
+}
